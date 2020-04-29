9066b5b1b40347017abc29502bdb7bc7f2d239b8
Qadeer Ashraf <qashraf@gmail.com>
2020-04-29 11:35:36 +0200

    Player with names

--- index.js
+++ index.js
@@ -20,13 +20,19 @@ function play() {
   
   //If player 1 wins
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Play White Wins!";
+    document.querySelector("h1").innerHTML = 
+    "🚩 " + document.getElementById("player1Name").value + " Wins!";
+
+    document.getElementById("player1Name").innerText;
   }
   else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player Brown Wins! 🚩";
+    document.querySelector("h1").innerHTML =
+    document.getElementById("player2Name").value + " Wins! 🚩";
   }
   else {
     document.querySelector("h1").innerHTML = "Draw!";
   }
 }
+
+
 
